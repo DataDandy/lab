a275260c0855d4e90babdc989a2b199e44fa3f3c
DataDandy <107078514+DataDandy@users.noreply.github.com>
2023-04-21 00:27:27 -0500

    Fully implemented the coding for account.py

--- account.py
+++ account.py
@@ -1,12 +1,24 @@
 class Account:
-    def __init__(self, name):
-        pass
+    def __init__(self, name: str) -> None:
+        self.__name = name
+        self.__balance = 0
 
     def deposit(self, amount):
-        pass
+        if amount > 0:
+            self.__balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if amount <= 0 or amount > self.__balance:
+            return False
+        else:
+            self.__balance -= amount
+            return True
 
     def get_balance(self):
-        pass
+        return self.__balance
 
     def get_name(self):
-        pass
+        return self.__name
